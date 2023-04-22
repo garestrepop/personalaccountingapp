f27058047f54316ec465d374bef17d03dee08ade
garestrepop <garestrepop@gmail.com>
2023-04-21 23:02:03 -0500

    v.0.0.0 update personalaccountingapp

---
+++ personalaccountingapp/src/main/java/com/grestrp/personalaccountingapp/PersonalaccountingappApplication.java
@@ -0,0 +1,13 @@
+package com.grestrp.personalaccountingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalaccountingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalaccountingappApplication.class, args);
+	}
+
+}
---
+++ personalaccountingapp/src/test/java/com/grestrp/personalaccountingapp/PersonalaccountingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grestrp.personalaccountingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalaccountingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
